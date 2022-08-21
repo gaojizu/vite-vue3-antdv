9aea56c5fb4256d1cee14513ce0611badc60c4d7
gaoajizu <wlm_0826@163.com>
2022-08-21 23:53:17 +0800

    fix: coe about config.js to axios and add setting about authorization

--- src/http/config.js
+++ src/http/config.js
@@ -9,30 +9,28 @@ console.log(import.meta.env)
 const config = {
     baseURL: import.meta.env.VITE_APP_BASE_URL, //配置区分环境
     timeOut: 30000,
-    // header: {
-    //     "Content-Type" : "multipart/form-data;charset=UTF-8",
-    //     "Access-Control-Allow-Origin-Type" : "*"
-    // }
+    header: {
+        "Content-Type": "application/json;charset=UTF-8",  //配置默认post请求头信息
+        "Access-Control-Allow-Origin-Type": "*", //允许跨域运行
+        Authorization: '687689698760879870',// 这里需要注意的token里面不可以出现中文，否则会直接报错 
+    }
 }
-
 const server = axios.create(config)
-const Token = new Date() //设置一个Token 临时的
 
-//配置默认post请求头信息
-server.defaults.headers.post["Content-Type"] = "multipart/form-data;charset=UTF-8";
-//允许跨域运行
-server.defaults.headers.post["Access-Control-Allow-Origin-Type"] = "*";
+//配置默认post请求头信息 上方直接配置 这里默认不配置
+// server.defaults.headers.post["Content-Type"] = "application/json;charset=UTF-8";
+// server.defaults.headers.post["Access-Control-Allow-Origin-Type"] = "*";
 
 //封装请求拦截器
 server.interceptors.request.use((config) => {
-    //进行Token的设置
-    // config.headers.common['Authorization'] = Token
+    // config当前的请求信息配置文件对象 可以进行数据的拦截处理
     return config
 }, (error) => {
     return Promise.reject(error)
 })
 //进行响应拦截
 server.interceptors.response.use((response) => {
+    console.log('res', response)
     // 2**范围内的都会触发该函数
     if (response.status == 200) {
         return Promise.resolve(response)
@@ -42,26 +40,6 @@ server.interceptors.response.use((response) => {
 }, (error) => {
     return Promise.reject(error)
 })
-
-/**
- * @params url 请求参数
- * @params method 请求方式
- * @params p 请求参数
- */
-// server((url, method, p) => {
-//     return new Promise((resolve, reject) => {
-//         const _Params = method == 'get' ? { params: p } : { data: p }
-//         axios({
-//             method,
-//             url,
-//             _Params
-//         }).then(res => {
-//             resolve(res)
-//         }).catch(err => {
-//             reject(err)
-//         })
-//     })
-// })
 
 //导出默认axios配置
 export default server
